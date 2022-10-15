4d02d844782c11eef0f5e7cffbe7efa07e4b5ef2
zhengjingwei <zhengjingwei@aliyun.com>
2022-10-15 19:33:08 +0800

    add parse template file in handler function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "net/http"
     "strconv"
+    "log"
+    "html/template"
 )
 
 // Define a home handler function which writes a byte slice containing 
@@ -14,7 +16,31 @@ func home(w http.ResponseWriter, r *http.Request) {
         http.NotFound(w, r)
         return
     }
-    w.Write([]byte("Hello from Snippetbox\n"))
+    // w.Write([]byte("Hello from Snippetbox\n"))
+    /* 
+     * Use the template.ParseFiles() functions to read the template file into a 
+     * template set. If there's an error, we log the detailed error message and use 
+     * the http.Error() funtion to send a generic 500 Internal Server Error 
+     * response to the user.
+     */
+    ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    /* 
+     * We then use the Execute() method on the template set to write the 
+     * template content as the response body. The last parameter to Execute() 
+     * represents any dynamic data that we want to pass in, which for now we'll 
+     * leave as nil.
+     */
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Print(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+    }
 }
 
 // Add a snippetView handler function. 
