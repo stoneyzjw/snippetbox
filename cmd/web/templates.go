37fefbc7333ec32589b3cc0652f43a3433e2c3e3
zhengjingwei <zhengjingwei@aliyun.com>
2022-10-16 21:29:42 +0800

    Rendering multiple pieces of data

---
+++ cmd/web/templates.go
@@ -0,0 +1,13 @@
+package main 
+
+import "github.com/stoneyzjw/snippetbox/internal/models" 
+
+/* 
+ * Define a templateData type to act as the holding structure for 
+ * any dynamic data that we want to pass to our HTML templates. 
+ * At the moment it only contains one field, but we'll add more 
+ * to it as the build progress. 
+ */ 
+type templateData struct {
+    Snippet *models.Snippet
+}
