6b41b99a92fa4e6cc5552bed2764f3cd588c0330
stoney <stoney@aliyun.com>
2022-10-17 15:16:19 +0800

    Custom error handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,6 +14,13 @@ import (
 func (app *application) routes() http.Handler {
 	// Initialize the router 
 	router := httprouter.New()
+	// Create a handler function which wraps our notFound() helper, and then 
+	// assign it as the custom handler for 404 Not Found responses. You can also 
+	// set a custom handler for 405 Method Not Allowed responses by setting 
+	// router.MethodNotAllowed in the same way too. 
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
 	// Update the pattern for the route for the static files.
     fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
