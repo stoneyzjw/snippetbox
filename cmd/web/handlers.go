083bdce7bc3ff798c640e9085290f159c6e993c0
zhengjingwei <zhengjingwei@aliyun.com>
2022-10-17 22:37:00 +0800

    finish the validate form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,9 +5,18 @@ import (
     "net/http"
     "strconv"
     "errors" 
+    "strings"
+    "unicode/utf8"
     "github.com/stoneyzjw/snippetbox/internal/models"
 	"github.com/julienschmidt/httprouter"
 )
+
+type snippetCreateForm struct {
+    Title       string 
+    Content     string 
+    Expires     int 
+    FieldErrors map[string]string
+}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// Because httprouter matches the "/" path exactly, we can now remove the 
@@ -72,20 +81,83 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new snippet..."))
+    data := app.newTemplateData(r)
+
+    app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// Checking if the request method is a POST is now superfluous and can be 
-	// removed, because this is done automatically by httprouter. 
-	title := "0 snail"
-	content := "0 snail\nClimb Mount Fuji, \nBut slowly, slowly!\n\n- Kobayashi Issa" 
-	expires := 7
-	id, err := app.snippets.Insert(title, content, expires) 
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-	// Update the redirect path to use the new clean URL format. 
+    /* 
+     * First we call r.ParseForm() which adds any ata in POST request bodies 
+     * to the r.PostForm map. This also works in the same way for PUT and PATCH 
+     * requests. If there are any errors, we use our app.ClientError() helper to 
+     * send a 400 Bad Request response to the user. 
+     */ 
+    err := r.ParseForm() 
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest) 
+        return
+    }
+    // Use the r.PostForm.Get() method to retrieve the title and content 
+    // from the r.PostForm map 
+    // title := r.PostForm.Get("title")
+    // content := r.PostForm.Get("content")
+    /* 
+     * The r.PostForm.Get() method always returns the form data as a *string*. 
+     * However, we're expecting our expires to be a number, and want to 
+     * represent it in our Go code as an integer. So we need to manually convert 
+     * the form data to an integer using strconv.Atoi(), and we send a 400 Bad 
+     * Request response if the conversion fails
+     */
+    expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
+
+    // Initialize a map to hold any validation errors for the form fields. 
+
+    form := snippetCreateForm {
+        Title:          r.PostForm.Get("title"), 
+        Content:        r.PostForm.Get("content"),
+        Expires:        expires, 
+        FieldErrors:    map[string]string{},
+    }
+    /*
+     * Check that the title is not blank and is not more than 100 
+     * characters long. If it fails either of those checks, add a message to the 
+     * errors map using the field name as the key.
+     */
+    if strings.TrimSpace(form.Title) == "" {
+        form.FieldErrors["title"] = "This field cannot be blank" 
+    } else if utf8.RuneCountInString(form.Title) > 100 {
+        form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
+    }
+
+    // Check that the Content value isn't blank 
+    if strings.TrimSpace(form.Content) == "" {
+        form.FieldErrors["content"] = "This field cannot be blank"
+    }
+
+    // Check the expires value matches one of the permitted values (1, 7 or 365)
+    if expires != 1 && expires != 7 && expires != 365 {
+        form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
+    }
+
+    // If there are any erros, dump them in a plain text HTTP response and 
+    // return from the handler 
+    if len(form.FieldErrors) > 0 {
+        data := app.newTemplateData(r)
+        data.Form = form
+        app.render(w, http.StatusUnprocessableEntity, "create.tmpl", data)
+        return 
+    }
+
+    id, err := app.snippets.Insert(form.Title, form.Content, form.Expires) 
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -82,4 +82,5 @@ type templateData struct {
     CurrentYear     int
     Snippet         *models.Snippet
     Snippets        []*models.Snippet
+    Form            any
 }
